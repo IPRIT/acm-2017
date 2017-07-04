dc201dd3f3c349e64ccac24731595b98f480cc00
Alex <ipritoflex@yandex.ru>
2017-07-04 15:42:00 +0300

    fix(filters): bug fixes

--- app/js/states/contest-item/filters.js
+++ app/js/states/contest-item/filters.js
@@ -14,6 +14,8 @@ angular.module('Qemy.filters.contest-item', [
 ])
     .filter('formatStartTime', [function() {
         return function (diffTime) {
+            var sign = diffTime < 0 ? '-' : '';
+            diffTime = Math.abs(diffTime);
             var allSeconds = Math.floor(diffTime / 1000),
                 seconds = allSeconds % 60,
                 minutes = Math.floor(allSeconds / 60),
@@ -21,7 +23,7 @@ angular.module('Qemy.filters.contest-item', [
             minutes %= 60;
             var zF = function (num) { return num >= 0 && num < 10 ? '0' + num : num;},
                 formatString = 'hh:mm:ss';
-            return formatString
+            return sign + formatString
                 .replace(/(hh)/gi, zF(hours))
                 .replace(/(mm)/gi, zF(minutes))
                 .replace(/(ss)/gi, zF(seconds));
