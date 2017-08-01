ca87a156c9d9dabd3a15096e539534a08b9b23a7
Alex <ipritoflex@yandex.ru>
2017-08-01 20:47:21 +0300

    feat(table-service): dev update

--- src/route/test/index.js
+++ src/route/test/index.js
@@ -1,17 +1,9 @@
 import express from 'express';
 import * as models from '../../models';
-import * as contestMethods from '../../route/contest/methods';
-import { Cell } from "../../services/table/cell";
 import { extractAllParams } from "../../utils/utils";
-import { ContestValue } from "../../services/table/contest-value";
-import { Row } from "../../services/table/row";
-import { ContestTable } from "../../services/table/table";
 import { TableManager } from "../../services/table/table-manager";
-import Promise from 'bluebird';
 
 const router = express.Router();
-
-let table;
 
 router.get('/', (req, res, next) => {
   return _test(
@@ -20,11 +12,6 @@ router.get('/', (req, res, next) => {
     res.json(result);
   }).catch(next);
 });
-
-/*let tableManager = new TableManager(6);
-tableManager.selfDestructSubject$.subscribe(tableDestroyed => {
-  console.log('Table table destroyed event:', tableDestroyed);
-});*/
 
 let tableManager = new TableManager(6);
 
