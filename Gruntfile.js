d52a329d6c7bd0e8c6d96f914bd04b1574488f26
Alex <ipritoflex@yandex.ru>
2017-12-16 18:09:14 +0300

    bug fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,9 +57,9 @@ module.exports = function(grunt) {
           sourceMap: true,
           sourceMapName: 'app/prod/js/build.prod.min.js.map',
           compress: {
-            // drop_console: true
+            drop_console: true
           },
-          // drop_console: true
+          drop_console: true
         },
         files: {
           'app/prod/js/build.prod.min.js': [
