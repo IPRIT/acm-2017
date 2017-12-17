d4fc94da722eaf279e60170b9d1d899fc3503538
Alex <ipritoflex@yandex.ru>
2017-12-17 03:08:42 +0300

    CJ Protection

--- app/js/init.js
+++ app/js/init.js
@@ -92,7 +92,7 @@ function initApplication () {
       if (window == window.top || window.chrome && chrome.app && chrome.app.window) {
         document.documentElement.style.display = 'block';
       } else {
-        //top.location = self.location;
+        window.write('CJ protection');
       }
     } catch (e) {console.error('CJ protection', e)};
 
