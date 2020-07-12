e0e4816e56192b45efc307a0b03b30502b90698a
Alexander Belov <ipritoflex@yandex.ru>
2020-07-12 23:55:08 +0300

    Update

--- src/systems/cf/account.js
+++ src/systems/cf/account.js
@@ -25,14 +25,10 @@ export async function login(systemAccount) {
     if (!response.body || ![ 200, 302 ].includes( response.statusCode )) {
       throw new Error('Service unavailable');
     }
-    console.log(response.body);
     const [ cookie, qs ] = getProtectionCookie(response.body);
-    console.log('https://codeforces.com/?' + qs, cookie);
 
     if (cookie) {
       jar.setCookie(cookie, 'https://codeforces.com');
-
-      console.log(jar);
 
       response = await request({
         method: 'GET',
